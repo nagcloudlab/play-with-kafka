99c5bdacc05cca9e0d6fb2fa85486cc1c2117db5
Nagabhushanam <nagabhushanam.n@revature.com>
2023-12-18 13:17:10 +0530

    day-6 commit

--- 04-transaction-consumer/src/main/java/com/example/service/RetryTransactionConsumerService.java
+++ 04-transaction-consumer/src/main/java/com/example/service/RetryTransactionConsumerService.java
@@ -6,6 +6,8 @@ import com.example.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.AcknowledgingMessageListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,56 +18,54 @@ import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
-public class RetryTransactionConsumerService /*implements AcknowledgingMessageListener<TransactionKey,Transaction> */ {
+public class TransactionConsumerService /*implements AcknowledgingMessageListener<TransactionKey,Transaction>*/ {
 
     private final TransactionRepository transactionRepository;
 
+
     @KafkaListener(
-            groupId = "DLT.transaction-consumer-group",
-            topics = "transactions.RETRY",
+            groupId = "transaction-consumer-group",
+            topics = "transactions",
             containerFactory = "kafkaListenerContainerFactory"
     )
     @Transactional
     public void onNewTransaction(ConsumerRecord<TransactionKey, Transaction> consumerRecord) {
-        System.out.println("Received RETRY transaction: " + consumerRecord.value());
+        System.out.println("Received new transaction: " + consumerRecord.value());
 
-//        Transaction transaction=consumerRecord.value();
-//        System.out.println(transaction);
+        validate(consumerRecord.value());
 
-//        validate(transaction);
-        com.example.entity.Transaction transaction = new com.example.entity.Transaction();
-        transaction.setId(consumerRecord.value().getId());
-        transaction.setType(consumerRecord.value().getType());
-        transaction.setAmount(consumerRecord.value().getAmount());
+        // Message/Event to Entity
+        com.example.entity.Transaction transactionEntity = new com.example.entity.Transaction();
+        transactionEntity.setId(consumerRecord.value().getId());
+        transactionEntity.setAmount(consumerRecord.value().getAmount());
+        transactionEntity.setType(consumerRecord.value().getType());
+        transactionEntity.setFromAccount(consumerRecord.value().getFromAccount());
+        transactionEntity.setToAccount(consumerRecord.value().getToAccount());
         Instant instant = consumerRecord.value().getTimestamp().atZone(ZoneId.systemDefault()).toInstant();
         Date date = Date.from(instant);
-        transaction.setTimestamp(date);
-        transaction.setFromAccount(consumerRecord.value().getFromAccount());
-        transaction.setToAccount(consumerRecord.value().getToAccount());
-        transactionRepository.save(transaction);
+        transactionEntity.setTimestamp(date);
+
+        transactionRepository.save(transactionEntity);  // .....
 
     }
 
     private void validate(Transaction transaction) {
-        if (transaction.getFromAccount() == null) {
-            throw new IllegalArgumentException("From account is null");
-        }
-        if (transaction.getToAccount() == null) {
-            throw new IllegalStateException("To account is null");
+        if(true){
+            throw new IllegalStateException("something bad happened"); // Transient failure
         }
     }
 
+
 //    @Override
 //    @KafkaListener(
+//            groupId = "transaction-consumer-group",
 //            topics = "transactions",
 //            containerFactory = "kafkaListenerContainerFactory"
 //    )
 //    public void onMessage(ConsumerRecord<TransactionKey, Transaction> consumerRecord, Acknowledgment acknowledgment) {
-//        System.out.println("Received new transaction: " + consumerRecord);
-//        acknowledgment.acknowledge();
+//        System.out.println("Received new transaction: " + consumerRecord.value());
+//        acknowledgment.acknowledge(); // manual ack ( commit offset )
 //    }
-
-
 
 
 }
