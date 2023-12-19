0e82ac7966675fdd5d239c8a6e8997ad73def9aa
nag <nagtraininglab@gmail.com>
2023-12-19 12:27:31 +0530

    kafka streams

--- kafka-streams/src/main/java/com/example/domain/Transaction.java
+++ kafka-streams/src/main/java/com/example/domain/Transaction.java
@@ -2,19 +2,13 @@ package com.example.domain;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Value;
+
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Value
-@Builder
-@NoArgsConstructor(force = true)
-@RequiredArgsConstructor
+
 public class Transaction {
     UUID id;
     String fromAccount;
@@ -23,4 +17,52 @@ public class Transaction {
     LocalDateTime timestamp;
     BigDecimal amount;
     TransactionType type;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFromAccount() {
+        return fromAccount;
+    }
+
+    public void setFromAccount(String fromAccount) {
+        this.fromAccount = fromAccount;
+    }
+
+    public String getToAccount() {
+        return toAccount;
+    }
+
+    public void setToAccount(String toAccount) {
+        this.toAccount = toAccount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
 }
--- kafka-streams/src/main/java/com/example/domain/TransactionKey.java
+++ kafka-streams/src/main/java/com/example/domain/TransactionKey.java
@@ -1,18 +1,36 @@
 package com.example.domain;
 
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Value;
+
 
 import java.util.UUID;
 
-@Builder
-@NoArgsConstructor(force = true)
-@RequiredArgsConstructor
-@Value
+
 public class TransactionKey {
     UUID id;
     String fromAccount;
     String toAccount;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFromAccount() {
+        return fromAccount;
+    }
+
+    public void setFromAccount(String fromAccount) {
+        this.fromAccount = fromAccount;
+    }
+
+    public String getToAccount() {
+        return toAccount;
+    }
+
+    public void setToAccount(String toAccount) {
+        this.toAccount = toAccount;
+    }
 }
